ca4fb5aabe7c49e8d19c8b6856ce5b4fd6406256
Arijit <subhasghosh196@outlook.com>
2022-09-17 20:00:29 +0530

    added new code

--- 03_linear_queue.c
+++ 03_linear_queue.c
@@ -25,14 +25,15 @@ int isEmpty(queue *ptr){
 
 void enqueue(queue *ptr, int elem){
     if(isFull(ptr))
-    return 0;
+    printf("Queue Overflow!");
     else{
         ptr->rear++;
         ptr->arr[ptr->rear]=elem;
+        printf("Element %d enqueued.\n",elem);
     }
 }
 
-int dequeue(queue *ptr, int elem){
+int dequeue(queue *ptr){
     if(isEmpty(ptr))
         return 0;
     else{
@@ -45,9 +46,30 @@ int dequeue(queue *ptr, int elem){
 
 int main(){
     queue * new=(struct queueNew *)malloc(sizeof(struct queueNew));
-    new->size=100;
+    new->size=4;
     new->front=-1;
     new->rear=-1;
     new->arr=(int *)malloc(new->size*sizeof(int));
+    if(isEmpty(new))
+    printf("Queue is empty.\n");
+    
+    enqueue(new, 10);
+    enqueue(new, 20);
+    enqueue(new, 30);
+    enqueue(new, 40);
+
+    printf("Dequeued element %d\n",dequeue(new));
+    printf("Dequeued element %d\n",dequeue(new));   
+    printf("Dequeued element %d\n",dequeue(new));   
+    printf("Dequeued element %d\n",dequeue(new));   
+
+
+    if(isEmpty(new))
+    printf("Queue is empty.\n");
+    
+    if(isFull(new))
+    printf("Queue is full.\n");
+
+    enqueue(new, 40);
     return 0;
 }
