0ab656adab0dd4b57962b9c83282024ba44e89ab
Arijit <subhasghosh196@outlook.com>
2022-11-01 21:44:35 +0530

    post and inorder

--- linked_rep_binaryTree.c
+++ linked_rep_binaryTree.c
@@ -25,6 +25,21 @@ void preorder( struct node * ptr){
         preorder(ptr->right);
     }
 }
+
+void postorder( struct node * ptr){
+    if(ptr!=NULL){
+        postorder(ptr->left);
+        postorder(ptr->right);
+        printf("%d ",ptr->data);
+    }
+}
+void inorder( struct node * ptr){
+    if(ptr!=NULL){
+        inorder(ptr->left);
+        printf("%d ",ptr->data);
+        inorder(ptr->right);
+    }
+}
 int main(){
     struct node *p,*p1,*p2,*p3,*p4;
 
@@ -56,9 +71,21 @@ int main(){
         // 5   2
         // PreOrder: 4[ ][ ]
         //           4[1[][]][6[][]]
-        //           4[1[5][]][6[][2]]
-        //           4 1 5 6 2
+        //           4[1[5][2]][6[][]]
+        //           4 1 5 2 6
+        // PostOrder: [ ][ ]4
+        //           [[][]1][[][]6]4
+        //           [[5][2]1][[][]6]4
+        //           5 2 1 6 4
+        // InOrder: [ ]4[ ]
+        //           [[]1[]]4[[]6[]]
+        //           [[5]1[2]]4[[]6[]]
+        //           5 1 2 4 6
     preorder(p);
+    printf("\n");
+    postorder(p);
+    printf("\n");
+    inorder(p);
 
     return 0;
 }
