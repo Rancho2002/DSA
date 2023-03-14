d54330e7bbd4f6bf456800457a38cb42d987e71f
arijit ghosh <subhasghosh196@outlook.com>
2023-03-15 01:31:28 +0530

    upto stack

---
+++ sololearn/tupleUnpacking.py
@@ -0,0 +1,21 @@
+# a, b, *c, d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+
+# a, b, c, d, *e, f, g = range(20)
+# print(len(e))
+letters = {"a", "b", "c", "d"}
+if "e" not in letters:
+  print(1)
+else: 
+  print(2)
+
+'''
+1
+2
+[3, 4, 5, 6, 7, 8]
+9
+'''
