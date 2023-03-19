9989e5e414b19121773b6ff397751d8e4be2a305
arijit ghosh <subhasghosh196@outlook.com>
2023-03-19 22:57:47 +0530

    modified find pivot

--- lovebabbar/findPivot.py
+++ lovebabbar/findPivot.py
@@ -1,15 +1,17 @@
 class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        n = len(nums)
-        p = [0] * n
-        s = [0] * n
-        p[0] = nums[0]
-        for i in range(1, n):
-            p[i] = p[i-1] + nums[i]
-        s[n-1] = nums[n-1]
-        for i in range(n-2, -1, -1):
-            s[i] = s[i+1] + nums[i]
+    def pivotIndex(self, arr: List[int]) -> int:
+        n=len(arr)
+        prefix=[0]*n
+        suffix=[0]*n
+        prefix[0]=arr[0]
+        for i in range(1,len(arr)):
+            prefix[i]=prefix[i-1]+arr[i] # 1 8 11 17 22 28
+
+        suffix[n-1]=arr[n-1]
+        for i in range(n-2,-1,-1):
+            suffix[i]=suffix[i+1]+arr[i] # 28 27 20 17 11 6
+
         for i in range(n):
-            if p[i] == s[i]:
+            if(prefix[i]==suffix[i]): 
                 return i
         return -1
