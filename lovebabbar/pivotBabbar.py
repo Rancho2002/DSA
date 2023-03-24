6fffe22713f516718c3027a31502583e088dda8f
Arijit <subhasghosh196@outlook.com>
2023-03-24 21:50:49 +0530

    added

---
+++ lovebabbar/pivotBabbar.py
@@ -0,0 +1,19 @@
+# 8 10 17 1 3 4
+
+def getPivot(arr,n):
+    s=0
+    e=n-1
+    mid=s+(e-s)//2
+    while(s<e):
+        if(arr[mid]>=arr[0]):
+            s=mid+1
+        else:
+            e=mid
+
+        mid=s+(e-s)//2 # (s+e)/2 10^6+(10^7-10^6)
+    return s
+
+if __name__=='__main__':
+    arr=[8,10,17,1,3,4]
+    ans=getPivot(arr,len(arr))
+    print(ans)
